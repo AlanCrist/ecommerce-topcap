3a648f4eb8ceeeb51bbc6f2e932775b97a8651a0
Alan <lanzn16@outlook.com>
2020-03-11 20:57:50 -0300

    add page home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,82 @@
 import React from 'react';
+import { MdAddShoppingCart } from "react-icons/md"
+import { ProductList } from "./styles";
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img src="//www.dhresource.com/webp/m/0x0s/f2-albu-g6-M01-08-63-rBVaSFuMg3CAAAWGAAC1BiLeTbA734.jpg/ajust-vel-casual-snapbacks-homens-maldita.jpg" alt="Cap" />
+        <strong>Cap black DAMN</strong>
+        <span>$ 40.99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="//www.dhresource.com/webp/m/0x0s/f2-albu-g6-M01-08-63-rBVaSFuMg3CAAAWGAAC1BiLeTbA734.jpg/ajust-vel-casual-snapbacks-homens-maldita.jpg" alt="Cap" />
+        <strong>Cap black DAMN</strong>
+        <span>$ 40.99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="//www.dhresource.com/webp/m/0x0s/f2-albu-g6-M01-08-63-rBVaSFuMg3CAAAWGAAC1BiLeTbA734.jpg/ajust-vel-casual-snapbacks-homens-maldita.jpg" alt="Cap" />
+        <strong>Cap black DAMN</strong>
+        <span>$ 40.99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="//www.dhresource.com/webp/m/0x0s/f2-albu-g6-M01-08-63-rBVaSFuMg3CAAAWGAAC1BiLeTbA734.jpg/ajust-vel-casual-snapbacks-homens-maldita.jpg" alt="Cap" />
+        <strong>Cap black DAMN</strong>
+        <span>$ 40.99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="//www.dhresource.com/webp/m/0x0s/f2-albu-g6-M01-08-63-rBVaSFuMg3CAAAWGAAC1BiLeTbA734.jpg/ajust-vel-casual-snapbacks-homens-maldita.jpg" alt="Cap" />
+        <strong>Cap black DAMN</strong>
+        <span>$ 40.99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="//www.dhresource.com/webp/m/0x0s/f2-albu-g6-M01-08-63-rBVaSFuMg3CAAAWGAAC1BiLeTbA734.jpg/ajust-vel-casual-snapbacks-homens-maldita.jpg" alt="Cap" />
+        <strong>Cap black DAMN</strong>
+        <span>$ 40.99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
