91a87225618bfca7cbcfcd72ea1982df1260a4f9
Alan <lanzn16@outlook.com>
2020-03-12 22:41:35 -0300

    update configuration store in enhancer development

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,10 @@ import { createStore } from "redux";
 
 import routReducer from "./modules/routReducer";
 
-const store = createStore(routReducer);
+const enhancer = process.env.NODE_ENV === "development"
+    ? console.tron.createEnhancer()
+    : null;
+
+const store = createStore(routReducer, enhancer);
 
 export default store;
