e807ed064c6f1de8378333b732dab2b0dd66ba0b
Alan <lanzn16@outlook.com>
2020-03-12 22:41:10 -0300

    add maps to props home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 import { MdAddShoppingCart } from "react-icons/md"
 import api from "../../services/api";
 import { ProductList } from "./styles";
+import * as CartActions from "../../store/modules/cart/actions";
 
 class Home extends React.Component {
   state = {
     products: []
-  }
+  };
 
   async componentDidMount() {
     const response = await api.get("products");
@@ -16,16 +18,14 @@ class Home extends React.Component {
   }
 
   handleAddProduct = product => {
-    const { dispatch } = this.props;
+    const { addToCart } = this.props;
 
-    dispatch({
-      type: "ADD_TO_CART",
-      product,
-    })
+    addToCart(product);
   }
 
   render() {
     const { products } = this.state;
+    const { amount } = this.props;
 
     return (
       <ProductList>
@@ -37,8 +37,8 @@ class Home extends React.Component {
 
             <button type="button" onClick={() => this.handleAddProduct(product)}>
               <div>
-                <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+                <MdAddShoppingCart size={16} color="#fff" /> {amount[product.id] || 0}
+              </div>
               <span>ADICIONAR AO CARRINHO</span>
             </button>
           </li>
@@ -48,4 +48,15 @@ class Home extends React.Component {
   }
 }
 
-export default connect()(Home);
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+
+    return amount;
+  }, {})
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
